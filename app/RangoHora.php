9e40660b00e96f185a2b7c8fe50522b8a864d178
Villanueva <xxcarlos489xx@gmail.com>
2020-12-18 23:50:36 -0500

    create model rango hora

---
+++ app/RangoHora.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class RangoHora extends Model
+{
+    protected $table = "rango_horas";
+}
--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\Estado;
 use App\Medico;
 use App\Paciente;
 use App\Usuario;
+use App\RangoHora;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Database\Eloquent\Model;
 /*
@@ -33,36 +34,44 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/login', function (Request $request) {
     
     $dni = $request->input('dni');
+    $maxDig = strlen($dni);
 	$pass = $request->input('pass');
 	$tipo = $request->input('tipo');
-
-	switch ($tipo) {
-		case 1:
-			$consulta = Paciente::where('dni',$dni)->first();
-			
-			break;
-		case 2:
-			$consulta = Medico::where('dni',$dni)->first();
-			break;
-		case 3:
-			$consulta = Empleado::where('dni',$dni)->first();
-			break;
-	}
-
-	if ($consulta) {
-		if($pass == $consulta->password){
-	        $response["dato"]=array('estado' => 200,'dato' => $consulta);
-	        echo json_encode($response["dato"]);
-	    }else{
-	        //header('Status: 400', TRUE, 400);
-	        $response["dato"]=array('estado' => 400,'message' =>'Contraseña no coincide' );
-	        echo json_encode($response["dato"]);
-	    }	
-	}else{
-	    //header('Status: 400', TRUE, 400);
-	    $response["dato"]=array('estado' => 400,'message' =>'Usuario no encontrado' );
-	    echo json_encode($response["dato"]);
-	}	
+    
+    if($maxDig ==8){
+        switch ($tipo) {
+    		case 1:
+    			$consulta = Paciente::where('dni',$dni)->first();
+    			
+    			break;
+    		case 2:
+    			$consulta = Medico::where('dni',$dni)->first();
+    			break;
+    		case 3:
+    			$consulta = Empleado::where('dni',$dni)->first();
+    			break;
+    	}
+    
+    	if ($consulta) {
+    		if($pass == $consulta->password){
+    	        $response["dato"]=array('estado' => 200,'dato' => $consulta);
+    	        echo json_encode($response["dato"]);
+    	    }else{
+    	        //header('Status: 400', TRUE, 400);
+    	        $response["dato"]=array('estado' => 400,'message' =>'Contraseña no coincide' );
+    	        echo json_encode($response["dato"]);
+    	    }	
+    	}else{
+    	    //header('Status: 400', TRUE, 400);
+    	    $response["dato"]=array('estado' => 400,'message' =>'Usuario no encontrado' );
+    	    echo json_encode($response["dato"]);
+    	}
+    }else{
+        $response["dato"]=array('estado' => 400,'message' =>'Dni debe contener 8 digitos' );
+    	echo json_encode($response["dato"]);
+    }
+    
+		
 	
 });
 
@@ -78,8 +87,11 @@ Route::post('/register', function (Request $request) {
 	$pass = $request->input('pass');
 	$tipo = $request->input('tipo');
 	$tel = $request->input('tel');
+    $maxDigDni = strlen($dni);
+    $maxDigTel = strlen($tel);
     
-	switch ($tipo) {
+    if( $maxDigDni == 8 && $maxDigTel == 9){
+        switch ($tipo) {
 		case 1:
 		    $consulta = Paciente::where("dni",$dni)->get();
 		    
@@ -132,7 +144,12 @@ Route::post('/register', function (Request $request) {
 		    }
 			break;
 		
-	}
+	    }
+    }else{
+        $response["dato"]=array('estado' => 400,'message' =>'Dni debe contener 8 digitos y número de teléfono debe contener 9 digitos' );
+    	echo json_encode($response["dato"]);
+    }
+	
 
 });
 
@@ -175,7 +192,7 @@ Route::get('/cita/{id}',function ($id, Request $request){
 //http://127.0.0.1:8000/api/especialidades
 Route::get('/especialidades',function (){
 
-	$consulta = Especialidad::select('id','nombre')->first();
+	$consulta = Especialidad::select('id','nombre')->get();
 	$response["dato"]=array('dato' => $consulta);
 
 	if ($consulta) {
@@ -186,11 +203,16 @@ Route::get('/especialidades',function (){
 /*==============================================
  * SPINNER LISTA MEDICOS POR ID DE ESPECIALIDAD
  ==============================================*/
-//http://127.0.0.1:8000/api/medicos/1
+//https://citas.dmqvirucida.com.pe/api/medicos/1
 Route::get('/medicos/{id}',function ($id){
 
-	$consulta = Medico::select('id','nombres','apellidos','consultorio_id','especialidad_id')->where('especialidad_id',$id)->get();
-
+	
+    $consulta = DB::table('medico')
+                    ->join('consultorio', 'medico.id', '=', 'consultorio.medico_id')
+                    ->select('medico.id','medico.nombres','medico.apellidos','medico.especialidad_id', 'consultorio.descripcion as consultorio','consultorio.id as id_consultorio')
+                    ->where('medico.especialidad_id','=',$id)
+                    ->get();
+                
 	$response["dato"]=array('dato' => $consulta);
 
 	if ($consulta) {
@@ -220,28 +242,69 @@ Route::get('/consultorios/{numero}/{id}',function ($numero,$id){
  * GENERAR CITA
  ==============================================*/
 
-Route::post('/reserva',function (Request $request){
-
-	$id_usuario = $request->input("id"); 	
+Route::post('/reserva',function (Request $request){    
+	$id_usuario = $request->input("paciente"); 	
 	$id_medico = $request->input("medico");
 	$id_consultorio = $request->input("consultorio");
 	$id_especialidad = $request->input("especialidad");
 	$fecha = $request->input("fecha");
 	$hora = $request->input("hora");
+    
 
+
+
+
+    $consultaFecha = Cita::where('fecha',$fecha)
+                            ->where('medico_id',$id_medico)
+                            ->where('especialidad_id',$id_especialidad)
+                            ->where('consultorio_id', $id_consultorio)
+                            ->get();
+    
+    if(count($consultaFecha)>0){
+        if(count($consultaFecha)==8){
+        	$response["dato"]=array('estado' => 400,'message' =>'No existe horario disponible para esta fecha' );
+    		echo json_encode($response["dato"]);
+    		return;
+        }
+
+        for ($i=0; $i < count($consultaFecha); $i++) { 
+        	if ($consultaFecha[$i]->paciente_id == intval($id_usuario)) {
+        		$rangoHora = RangoHora::where('id',intval($hora))->first();
+        		$rangoHora = $rangoHora->rango;
+        		$message = 'Ya tiene una cita reservada para la fecha '+ $consultaFecha[$i]->fecha + 'en el horario de '
+        					+ $rangoHora;
+        		$response["dato"]=array('estado' => 400,'message' =>$message);
+    			echo json_encode($response["dato"]);
+    			return;
+        	}
+        }
+
+        for ($i=0; $i < count($consultaFecha); $i++) { 
+        	if ($consultaFecha[$i]->rangohora_id == intval($hora)) {
+        		$response["dato"]=array('estado' => 400,'message' =>'Este horario ya se encuentra reservado, seleccione otro horario');
+    			echo json_encode($response["dato"]);
+    			return;
+        	}
+        }        
+    }    
+    
 	$object = new Cita();
-	$object->paciente_id = $id_usuario;
-	$object->medico_id = $id_medico;
+	$object->paciente_id = intval($id_usuario);
+	$object->medico_id = intval($id_medico);
 	$object->estado = 1;
-	$object->consultorio_id = $id_consultorio;
-	$object->especialidad_id = $id_especialidad;
+	$object->consultorio_id = intval($id_consultorio);
+	$object->especialidad_id = intval($id_especialidad);
 	$object->fecha = $fecha;
-	$object->hora = $hora;
+	$object->rangohora_id = intval($hora);	
 	
 	if ($object->save()) {
-		echo json_encode("true");
+		$response["dato"]=array('estado' => 200,'message' =>'Su reserva ha sido generada con éxito');
+    	echo json_encode($response["dato"]);
+    	return;
 	}else{
-		echo json_encode("false");
+		$response["dato"]=array('estado' => 400,'message' =>'Ocurrio un error, intente nuevamente...');
+    	echo json_encode($response["dato"]);
+    	return;
 	}
 });
 
